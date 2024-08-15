2940c523f004c611b8e1cf7ce860d79cabcf8ef2
wericy <3679659+wericy@users.noreply.github.com>
2024-08-15 18:32:22 -0400

    fix linting TDD

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,18 +6,18 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
-    """Test the calc module. """
+    """Test the calc module."""
 
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test subtracting number."""
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
-        
